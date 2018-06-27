985db7ac5c0c7cd555e7620bafd60c67aee2e9b3
terrygonguet <terry@gonguet.com>
2018-06-27 12:42:36 +0200

    Fixed

--- keystone.js
+++ keystone.js
@@ -49,8 +49,6 @@ keystone.set('routes', require('./routes'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-	posts: ['posts', 'post-categories'],
-	galleries: 'galleries',
 	enquiries: 'enquiries',
 	users: 'users',
 });
