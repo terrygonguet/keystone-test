9d42de1ea825b06a409edf56faceab4ca83813fe
Daydreel <daydreel@gmail.com>
2018-07-03 22:56:13 +0200

    CSS about

--- routes/middleware.js
+++ routes/middleware.js
@@ -21,6 +21,7 @@ exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
+		{ label: 'Nous: le bon goût', key: 'about', href: '/about' },
 	];
 	res.locals.user = req.user;
 	next();
