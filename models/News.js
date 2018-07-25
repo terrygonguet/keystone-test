6ae5b5acb75cd9a4b0efc57720d6df6bbb1dd537
terrygonguet <terry@gonguet.com>
2018-07-25 15:36:17 +0200

    Fixed News Model

--- models/News.js
+++ models/News.js
@@ -9,7 +9,7 @@ var News = new keystone.List('News');
 
 News.add({
 	name: { type: Types.Text, required: true, initial: true, label: 'Title' },
-	content: { type: Types.Html, required: true, wysiwyg: true, initial: false },
+	content: { type: Types.Html, wysiwyg: true, initial: false },
 	state: { type: Types.Select, options: [
 		{ label: 'Draft', value: 'draft' },
 		{ label: 'Published', value: 'published' },
