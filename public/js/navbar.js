b319a538d4f9b4320ea57b2cafb08f695da7ee96
terrygonguet <terry@gonguet.com>
2018-07-25 15:47:44 +0200

    Improved train bar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -6,18 +6,24 @@ $(document).ready(function () {
 	let trainPos = trainWidth / 2;
 	let speed = 800;
 
-	rail.hover(function (e) {
+	function onInterraction (e) {
+		// only if we hover/move over the rail itself
+		if (e.target !== rail[0]) return;
 		let left = e.clientX - trainWidth / 2;
+		// keep the navbar in the screen
 		if (left < 0) {
 			left = 0;
 		} else if (left > innerWidth - trainWidth) {
 			left = innerWidth - trainWidth;
 		}
+		// compute duration to simulate constant speed
 		let duration = Math.abs(left - trainPos) / speed * 1000;
 		train.stop().animate({ left }, {
 			duration,
 			easing: 'linear',
-			step: (now) => (trainPos = now),
+			step: (now) => (trainPos = now), // keep track of where the bar is if the animation is interrupted
 		});
-	});
+	}
+
+	rail.hover(onInterraction).mousemove(onInterraction);
 });
