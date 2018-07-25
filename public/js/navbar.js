168b6c52e818b78009a0c6f54e1ebb2ae27e41bc
terrygonguet <terry@gonguet.com>
2018-07-25 17:29:46 +0200

    Cleaned up and improved homepage HTML & CSS Fixed nav train

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,18 +3,19 @@ $(document).ready(function () {
 	let rail = $('#trainRail');
 	let train = $('#navigationList');
 	let trainWidth = train.width();
+	let bounds = { start: rail.position().left, end: rail.position().left + trainWidth };
 	let trainPos = trainWidth / 2;
 	let speed = 800;
 
 	function onInterraction (e) {
 		// only if we hover/move over the rail itself
 		if (e.target !== rail[0]) return;
-		let left = e.clientX - trainWidth / 2;
+		let left = e.clientX - trainWidth / 2 - bounds.start;
 		// keep the navbar in the screen
 		if (left < 0) {
 			left = 0;
-		} else if (left > innerWidth - trainWidth) {
-			left = innerWidth - trainWidth;
+		} else if (left > rail.width() - trainWidth) {
+			left = rail.width() - trainWidth;
 		}
 		// compute duration to simulate constant speed
 		let duration = Math.abs(left - trainPos) / speed * 1000;
