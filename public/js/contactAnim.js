40f41c0f1d17315666e118ec7973961fd7bdd34c
terrygonguet <terry@gonguet.com>
2018-09-06 06:48:55 +0200

    Fixed a bunch of package errors and finished contact animation

--- public/js/contactAnim.js
+++ public/js/contactAnim.js
@@ -3,6 +3,7 @@ let userImgs = ['user', 'monitor', 'tablet', 'mobile-phone'];
 let users = [];
 let letters = [];
 let speed = 400; // px/s
+let timeSinceLastTick = 0;
 let timeSinceLastLetter = 0;
 let stage = new createjs.Stage('contactAnim');
 let dim = {
@@ -13,32 +14,41 @@ stage.canvas.width = dim.w;
 stage.canvas.height = dim.h;
 
 let logo = new createjs.Shape();
-let logoPos = $V([dim.w * 0.5, dim.h * 0.2]);
+let logoPos = $V([dim.w * 0.5, dim.h * 0.5]);
 logo.graphics.f('black').r(-16, -16, 32, 32);
 logo.x = logoPos.e(1);
 logo.y = logoPos.e(2);
 stage.addChild(logo);
 
-for (let i = 0; i < 10 + Math.floor(Math.random() * 5); i++) {
-	// choose an image and a position at random
-	let userImg = userImgs[Math.floor(Math.random() * userImgs.length)];
-	let user = new createjs.Bitmap(`images/contact/${userImg}.png`);
-	user.x = 0.05 * dim.w + Math.random() * 0.9 * dim.w;
-	user.y = 0.6 * dim.h + Math.random() * 0.35 * dim.h;
-	users.push(user);
-	stage.addChild(user);
-}
+let nbUsers = 15 + Math.floor(Math.random() * 5);
+
+do {
+	let userPos = $V([
+		0.05 * dim.w + Math.random() * 0.9 * dim.w,
+		0.15 * dim.h + Math.random() * 0.8 * dim.h,
+	]);
+	// if the point is too close to the logo or other users we don't create the user
+	let isClose = users.find(u => userPos.distanceFrom($V([u.x, u.y])) <= 60);
+	if (!isClose && userPos.distanceFrom(logoPos) >= 150) {
+		let userImg = userImgs[Math.floor(Math.random() * userImgs.length)];
+		let user = new createjs.Bitmap(`images/contact/${userImg}.png`);
+		user.set({ x: userPos.e(1), y: userPos.e(2) });
+		users.push(user);
+		stage.addChild(user);
+	}
+} while (users.length < nbUsers);
 
 // update the stage at 60FPS
 createjs.Ticker.timingMode = createjs.Ticker.RAF;
 createjs.Ticker.addEventListener('tick', function (e) {
-	timeSinceLastLetter += e.delta / 1000;
-	if (Math.random() < timeSinceLastLetter) {
-		timeSinceLastLetter = 0;
+	timeSinceLastTick += e.delta;
+	timeSinceLastLetter += e.delta;
+	if (timeSinceLastTick > 1000 / 20 && timeSinceLastLetter > 1000 && Math.random() < 0.1) {
+		timeSinceLastTick = timeSinceLastLetter = 0;
 		let letter = new createjs.Bitmap('images/contact/email.png');
 		let user = users[Math.floor(Math.random() * users.length)];
 		letter.set({
-			x: user.x, y: user.y, regX: 16,
+			x: user.x, y: user.y, regX: 16, regY: 16,
 		});
 		letters.push(letter);
 		stage.addChild(letter);
