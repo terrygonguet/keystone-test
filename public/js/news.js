05888f9397faa5eafba6a12914d4fddf691164ad
terrygonguet <terry@gonguet.com>
2018-09-12 15:35:55 +0200

    fixed display bug in news on language change

--- public/js/news.js
+++ public/js/news.js
@@ -1,59 +1,69 @@
 /* globals $ moment */
-var data = JSON.parse($('#newsData').val());
-
-$('.articleWrapper').click(function (e) {
-	let _id = $(this).data('id');
-	let article = data.find(a => a._id === _id);
-	let offset = $(this).offset();
-	let reader = $('#TheReaderWrapper');
-	let readerOffset = reader.offset();
-
-	$('.articleWrapper').removeClass('selected');
-	$(this).addClass('selected');
-
-	let clone = $(this).clone(false);
-	clone
-		.css({
-			position: 'absolute',
-			top: offset.top,
-			left: offset.left,
-			margin: 0,
-		})
-		.appendTo('body')
-		.animate({
-			top: readerOffset.top,
-			left: readerOffset.left,
-			width: reader.width(),
-			height: reader.height(),
-			backgroundColor: reader.css('background-color'),
-			color: reader.css('color'),
-			paddingRight: 0,
-			paddingBottom: 0,
-		}, {
-			duration: 700,
-			done: () => {
-				$('#readerName').text(article.name);
-				$('#readerPublished').text(moment(article.publishedAt).format('Do MMM YYYY'));
-				$('#readerContent').html(article.content);
-				clone.fadeOut(() => clone.detach());
-
-				let path = location.pathname;
-				if (path.endsWith('/news')) {
-					path += '/' + _id;
-				} else if (path.endsWith('/news/')) {
-					path += _id;
-				} else {
-					path = path.replace(/news\/(.+?)$/, 'news/' + _id);
-				}
-				history.pushState(null, '', path);
-			},
-		});
-});
-
-// load default article or first
 $(document).ready(function () {
+	var data = JSON.parse($('#newsData').val());
+
+	// article switching animation
+	$('.articleWrapper').click(function (e) {
+		let _id = $(this).data('id');
+		let article = data.find(a => a._id === _id);
+		let offset = $(this).offset();
+		let reader = $('#TheReaderWrapper');
+		let readerOffset = reader.offset();
+
+		$('.articleWrapper').removeClass('selected');
+		$(this).addClass('selected');
+
+		let clone = $(this).clone(false);
+		clone
+			.css({
+				position: 'absolute',
+				top: offset.top,
+				left: offset.left,
+				margin: 0,
+			})
+			.appendTo('body')
+			.animate({
+				top: readerOffset.top,
+				left: readerOffset.left,
+				width: reader.width(),
+				height: reader.height(),
+				backgroundColor: reader.css('background-color'),
+				color: reader.css('color'),
+				paddingRight: 0,
+				paddingBottom: 0,
+			}, {
+				duration: 700,
+				done: () => {
+					$('#readerName').text(article.name);
+					$('#readerPublished').text(moment(article.publishedAt).format('Do MMM YYYY'));
+					$('#readerContent').html(article.content);
+					clone.fadeOut(() => clone.detach());
+
+					setIdInURL(_id);
+				},
+			});
+	});
+
+	function setIdInURL (_id) {
+		let path = location.pathname;
+		if (path.endsWith('/news')) {
+			path += '/' + _id;
+		} else if (path.endsWith('/news/')) {
+			path += _id;
+		} else {
+			path = path.replace(/news\/(.+?)$/, 'news/' + _id);
+		}
+		history.pushState(null, '', path);
+	}
+
+	// load default article or first
 	let _id = $('#TheReaderWrapper').data('default-news');
 	let article = _id ? data.find(a => a._id === _id) : data[0];
+	if (!article) {
+		article = data[0];
+		$('.articleWrapper:eq(0)').addClass('selected');
+		setIdInURL(article._id);
+	}
 	$('#readerName').text(article.name);
 	$('#readerPublished').text(moment(article.publishedAt).format('Do MMM YYYY'));
 	$('#readerContent').html(article.content);
